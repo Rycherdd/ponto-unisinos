71c43923d5c5d347e33c16405594bce050c9b018
Gabriel Henz <gabriel.santos@zallpy.com>
2024-03-26 22:54:48 -0300

    refactor: rename points keys

--- server/src/routes/point.js
+++ server/src/routes/point.js
@@ -1,34 +1,34 @@
 import fp from "fastify-plugin";
 import { verifyToken } from "../middlewares/verifyToken.js";
 
-const pontos = [
+const points = [
   {
-    horarios: ["08:00", "12:00", "13:00", "17:00"],
-    idUsuario: "abc123",
+    times: ["08:00", "12:00", "13:00", "17:00"],
+    idUser: "abc123",
     date: "2024-03-26",
     created_at: "2024-03-26T08:15:00",
   },
   {
-    horarios: ["07:30", "12:30", "13:30", "17:30"],
-    idUsuario: "abc123",
+    times: ["07:30", "12:30", "13:30", "17:30"],
+    idUser: "abc123",
     date: "2024-03-26",
     created_at: "2024-03-26T07:30:00",
   },
   {
-    horarios: ["09:00", "12:15", "13:45", "18:00"],
-    idUsuario: "abc123",
+    times: ["09:00", "12:15", "13:45", "18:00"],
+    idUser: "abc123",
     date: "2024-03-27",
     created_at: "2024-03-27T09:20:00",
   },
   {
-    horarios: ["08:45", "12:45", "14:00", "17:45"],
-    idUsuario: "abc123",
+    times: ["08:45", "12:45", "14:00", "17:45"],
+    idUser: "abc123",
     date: "2024-03-27",
     created_at: "2024-03-27T08:50:00",
   },
   {
-    horarios: ["08:30", "12:30", "13:45", "17:30"],
-    idUsuario: "abc123",
+    times: ["08:30", "12:30", "13:45", "17:30"],
+    idUser: "abc123",
     date: "2024-03-28",
     created_at: "2024-03-28T08:30:00",
   },
@@ -39,14 +39,9 @@ async function pointRoutes(fastify, options) {
     "/point_register",
     { preHandler: verifyToken },
     async (request, reply) => {
-      const { usuario } = request;
-      const ponto = request.body.ponto;
-      if (usuario && ponto) {
-        pontos.push({ usuario, ponto });
-        reply.code(200).send({ message: "Ponto registrado com sucesso" });
-      } else {
-        reply.code(400).send({ message: "Parâmetros inválidos" });
-      }
+      const point = request.body;
+      points.push({ point });
+      reply.code(200).send({ message: "Point stored successfully" });
     }
   );
 
@@ -54,7 +49,7 @@ async function pointRoutes(fastify, options) {
     "/points_history",
     // { preHandler: verifyToken },
     async (request, reply) => {
-      reply.code(200).send(pontos);
+      reply.code(200).send(points);
     }
   );
 }
