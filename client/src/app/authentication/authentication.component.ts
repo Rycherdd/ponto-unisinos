7edce966b0871837d03a6b5e98530fa4d3f92db7
Gabriel Henz <gabriel.santos@zallpy.com>
2024-03-31 21:33:18 -0300

    feat(client): add auth form and real auth on mongo

--- client/src/app/authentication/authentication.component.ts
+++ client/src/app/authentication/authentication.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {ToastController} from "@ionic/angular";
+
 
 @Component({
   selector: 'app-authentication',
@@ -10,12 +13,31 @@ export class AuthenticationComponent {
   user: string = "";
   password: string = "";
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private http: HttpClient, private toastController: ToastController) {
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastController.create({
+      position: 'top',
+      color: 'danger',
+      message: message,
+      duration: 2000
+    });
+    await toast.present();
   }
 
   authUser() {
     console.log(this.user, this.password);
-    this.router.navigate(['/tabs/tab1']);
+    this.http.post('http://localhost:3000/auth', {user: this.user, password: this.password}).subscribe((res: any) => {
+      console.log(res);
+      if (res.token) {
+        // localStorage.setItem('token', res.token);
+        // localStorage.setItem('user', this.user);
+        this.router.navigate(['/tabs/points']);
+      }
+    }, async () => {
+      await this.showToast('User is incorrect');
+    });
   }
 
 }
